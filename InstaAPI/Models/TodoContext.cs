fe110bca7d9d0d9629c99e74f5c5891af0ca8455
Houman Ghahremanlou <houmi@Houmans-MacBook-Pro.local>
2019-11-14 18:03:25 -0800

    Adding Controller/Model

---
+++ InstaAPI/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InstaAPI.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> todoItems { get; set; }
+    }
+}
